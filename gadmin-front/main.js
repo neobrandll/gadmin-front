7b71b4b204667ca33c2e928f37a8c4efa3852835
Brandon Lugo <neobrandll@gmail.com>
2019-08-26 03:22:56 -0400

    electron added!

---
+++ gadmin-front/main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow, Menu } = require('electron');
+
+let win;
+
+const createWindow = () => {
+  win = new BrowserWindow({
+    // width: 1200,
+    // height: 800,
+    webPreferences: { backgroundThrottling: false },
+    icon: __dirname + '/src/assets/icon.png',
+    // resizable: false,
+    center: true
+  });
+
+  win.loadURL(__dirname + '/dist/index.html');
+  win.maximize();
+  win.on('closed', () => {
+    win = null;
+  });
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+
+  //win.webContents.openDevTools();
+  // process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = true;
+};
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+const menuTemplate = [
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'Quit',
+        accelerator: process.platform === 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
+
+if (process.platform === 'darwin') {
+  menuTemplate.unshift({});
+}
---
+++ gadmin-front/src/app/material/material.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+
+const Material = [];
+
+@NgModule({
+  imports: [Material],
+  exports: [Material]
+})
+export class MaterialModule {}
