8e20a9b9a6603becebfc53140e87876e349ba7c5
Brandon Lugo <neobrandll@gmail.com>
2019-08-30 04:13:56 -0400

    working with raza

---
+++ gadmin-front/src/app/raza/components/raza-page/raza-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-raza-page',
+  templateUrl: './raza-page.component.html',
+  styleUrls: ['./raza-page.component.scss']
+})
+export class RazaPageComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
